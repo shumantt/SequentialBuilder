3ebbc34e99864d1d904f1b48094070fe10e5a441
Andrey Shumeev <shumantt@gmail.com>
2021-04-24 15:24:02 +0300

    field generates its set code

--- SequentialBuilder/Model/BuilderField.cs
+++ SequentialBuilder/Model/BuilderField.cs
@@ -1,11 +1,12 @@
 using System;
-using System.Globalization;
 using System.Linq;
 
 namespace SequentialBuilder.Model
 {
     public class BuilderField
     {
+        private Lazy<string> camelCaseName;
+
         public BuilderField(string name, string typeName, int? order = null)
         {
             if(string.IsNullOrEmpty(name))
@@ -22,12 +23,21 @@ namespace SequentialBuilder.Model
             Name = name;
             TypeName = typeName;
             Order = order;
+            camelCaseName = new Lazy<string>(() => Name.First().ToString().ToUpper() + Name.Substring(1));
         }
         
         public string Name { get; }
         public string TypeName { get; }
         public int? Order { get; }
 
-        public string CamelCaseName => Name.First().ToString().ToUpper() + Name.Substring(1);
+        public string CamelCaseName => camelCaseName.Value;
+
+        public string GetSetCode()
+        {
+            return $@"
+                this.{Name} = {Name};
+                return this;
+            ";
+        }
     }
 }
